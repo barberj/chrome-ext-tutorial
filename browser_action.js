c59b7ead18fc5d58b3efe850d7b9347b5097ee8a
Justin Barber <barber.justin@gmail.com>
2016-05-03 22:34:38 -0400

    got cookies and sessions working in ext

--- browser_action.js
+++ browser_action.js
@@ -1,9 +1,29 @@
-(function (document) {
+(function(document) {
   function renderStatus(statusText) {
     document.getElementById('status').textContent = statusText;
   }
 
   document.addEventListener('DOMContentLoaded', function() {
+    let oReq = new XMLHttpRequest();
+    oReq.open('GET', 'http://localhost:3000/spike/user');
+    oReq.responseType = 'json';
+
+    oReq.onload = function() {
+      // Parse and process the response from Google Image Search.
+      let response = oReq.response;
+      if (!response) {
+        renderStatus('No response!');
+        return;
+      }
+
+      console.log(response);
+      renderStatus(`Ok ${response.ok}`);
+    };
+    oReq.onerror = function() {
+      renderStatus('Network error.');
+    };
+    oReq.send();
+
     renderStatus('Ready');
   });
 })(document);
